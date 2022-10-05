baa83e8bc17439aecf8ee350edbaeaf5c83ff33e
Vrixic21 <vrijpatel@icloud.com>
2022-10-05 14:52:23 -0400

    Read GameLevel.txt file code in main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,104 @@
 // With what we want & what we don't defined we can include the API
 #include "Gateware.h"
 #include "renderer.h"
+#include <fstream>
+
 // open some namespaces to compact the code a bit
 using namespace GW;
 using namespace CORE;
 using namespace SYSTEM;
 using namespace GRAPHICS;
+
+GW::MATH::GVECTORF ReadVectorFromString(std::string& str)
+{
+	GW::MATH::GVECTORF V = {};
+	unsigned int VIndex = 0;
+	std::string Num = "";
+
+	for (unsigned int i = 0; i < str.length(); ++i)
+	{
+		unsigned int AcsiiChar = static_cast<unsigned int>(str[i]);
+		if (str[i] == ',')
+		{
+			V.data[VIndex++] = std::atof(Num.c_str());
+			Num.clear();
+		}
+		else if ((AcsiiChar >= 48 && AcsiiChar <= 57) || AcsiiChar == 46 || AcsiiChar == 45)
+		{
+			Num.push_back(str[i]);
+		}
+	}
+
+	return V;
+}
+
+std::vector<GW::MATH::GMATRIXF>& ReadGameLevelFile()
+{
+	std::vector<GW::MATH::GMATRIXF> Matrices;
+
+	std::ifstream FileHandle("../GameLevel.txt");
+	std::string Line = "";
+	GW::MATH::GMATRIXF Matrix;
+
+	std::cout << "Opening File: GameLevel.txt\n";
+	if (FileHandle.is_open())
+	{
+		std::cout << "File Opened: GameLevel.txt\n";
+		while (true)
+		{
+			std::getline(FileHandle, Line);
+
+			if (FileHandle.eof()) { break; }
+
+			if (std::strcmp(Line.c_str(), "MESH") == 0)
+			{
+				/* Read Mesh Infomation */
+				std::cout << "[Mesh Found]: ";
+
+				std::getline(FileHandle, Line, '\n');
+
+				std::cout << Line << "\n";
+
+				/* Read the Matrix */
+
+				// Read Matrix Header
+				std::getline(FileHandle, Line, '(');
+				std::cout << Line << "("; // Print Matrix Header
+
+				std::getline(FileHandle, Line, ')'); // read row 1
+				std::cout << Line << ")";
+				Matrix.row1 = ReadVectorFromString(Line);
+
+				std::getline(FileHandle, Line, ')'); // read row 2
+				std::cout << Line << ")";
+				Matrix.row2 = ReadVectorFromString(Line);
+
+				std::getline(FileHandle, Line, ')'); // read row 3
+				std::cout << Line << ")";
+				Matrix.row3 = ReadVectorFromString(Line);
+
+				std::getline(FileHandle, Line, ')'); // read row 4
+				std::cout << Line << ")>\n\n";
+				Matrix.row4 = ReadVectorFromString(Line);
+
+				Matrices.push_back(Matrix);
+			}
+		}
+
+		FileHandle.close();
+	}
+
+	return Matrices;
+}
+
 // lets pop a window and use Vulkan to clear to a red screen
 int main()
 {
 	GWindow win;
 	GEventResponder msgs;
 	GVulkanSurface vulkan;
+
+	ReadGameLevelFile();
 
 	if (+win.Create(0, 0, 800, 600, GWindowStyle::WINDOWEDBORDERED))
 	{
